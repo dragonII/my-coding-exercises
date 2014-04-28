ad29f587e02959d3d20a8fb59abcaeac5364e8d4
wanglong1982@gmail.com <wanglong1982@gmail.com@05f01ab0-360f-b9ba-9408-f6320d514742>
2014-04-28 04:35:13 +0000

    Basic PeoTrix Arch

---
+++ Peotrix/src/core/peotrix.c
@@ -0,0 +1,9 @@
+/*
+ * Peo Matrix
+ */
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
---
+++ Peotrix/src/core/ptrx_conf_file.h
@@ -0,0 +1,16 @@
+#ifndef __PTRX_CONF_H__
+#define __PTRX_CONF_H__
+
+#include "ptrx_string.h"
+
+struct ptrx_open_file_s
+{
+    int             fd;
+    ptrx_str_t      name;
+    unsigned char   *buffer;
+    unsigned char   *pos;
+    unsigned char   *last;
+};
+
+
+#endif
