f9bc0bc9c623e8ce17c2b94502ae1067f36740dc
wanglong1982@gmail.com <wanglong1982@gmail.com@05f01ab0-360f-b9ba-9408-f6320d514742>
2014-06-16 02:46:06 +0000

    Add web exercise for head first Python

---
+++ python/hf_webapp_exercise/simple_httpd.py
@@ -0,0 +1,11 @@
+
+#from http.server import HTTPServer, CGIHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
+from CGIHTTPServer import CGIHTTPRequestHandler
+
+port = 12345
+
+httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
+print("Starting simple_httpd on port: " + str(httpd.server_port))
+httpd.serve_forever()
+
